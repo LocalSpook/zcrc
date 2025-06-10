a6d1e8cd4b44ee246b4cc4d0f306f6e005810567
Victor Chernyakin <chernyakin.victor.j@outlook.com>
2025-06-10 03:35:40 -0700

    Add breadcrumb trail back to this repository
    If we're going to encourage people to copy and
    paste the main header, we should make it clear
    where it originated from.

--- include/crc/crc.hpp
+++ include/crc/crc.hpp
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+// Homepage:  https://github.com/LocalSpook/crc
+//
 // ────────── Design notes ──────────
 //
 // To understand this library's internals, read the following papers:
